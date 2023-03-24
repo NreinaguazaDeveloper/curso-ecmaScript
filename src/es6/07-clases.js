85abeec1f52d3e89d995c284ab0c9d5fb4837a6b
Camilo Guaza <camiloguaza@gmail.com>
2023-03-23 20:04:21 -0500

    cambios de modulos

--- src/es6/07-clases.js
+++ src/es6/07-clases.js
@@ -43,3 +43,36 @@ class USER{
         return `${this.speak} ${this.name}`;
     }
 }
+
+const ana = new user ('Ana');
+console.log(ana.greeting());
+
+
+//Setters getters
+class user{
+    //CONSTRUCTOR
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+        //METODOS   
+    speak(){
+        return 'Hello';
+    }
+    greeting(){
+        return `${this.speak} ${this.name}`;
+
+    }
+    get uAge() {
+        return this.age;
+    }
+    set uAge(n){
+        this.age = n;
+    }
+}
+
+
+const bebeloper1 = new user ('David', 15);
+
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
---
+++ src/es6/08-module.mjs
@@ -0,0 +1,6 @@
+//importar los elementos de module.
+
+import hello  from "./module.mjs";
+hello();
+
+//añadir un flag de package.js
