9cb1d6698679a36146589ff5aed1ee79d9f03805
Camilo Guaza <camiloguaza@gmail.com>
2023-04-04 16:09:18 -0500

    Cambios hasta la clase 17 - playground

---
+++ src/es6/09-generator.js
@@ -0,0 +1,11 @@
+//nos va a retornar una serie de valores segun el algoritmo
+function* iterate(array){
+    for (let value of array){//Por cada elemento del array vamos a hacer algo
+        yield value;//yield va a retornar algo depende de lo que mandemos
+    }
+
+}
+
+const it = iterate(['Anuel','Carl','JH','Dross','Carolina'])
+
+console.log(it.next().value); //Esto solo nos muestra el primer valor.
